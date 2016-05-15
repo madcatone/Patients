e93c34b9ab1b47345ebffe3c162f53773e0ebcdd
rubyinhell <rubyinhell@gmail.com>
2016-05-16 00:23:52 +0800

    fix config/applocation.rb for locale

--- config/application.rb
+++ config/application.rb
@@ -15,13 +15,19 @@ module Blog
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = 'Taipei'
+    #config.time_zone = 'Taipei'
+    config.time_zone = 'Beijing'
+    config.active_record.default_timezone = :local
+    # config.i18n.default_locale = :zh
+    #config.i18n.default_locale = "zh-TW"
+    config.i18n.default_locale = "zh-CN"
+    config.encoding = "utf-8"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.default_locale = "zh-TW"
-    config.encoding = "utf-8"
+    #config.i18n.default_locale = "zh-TW"
+    #config.encoding = "utf-8"
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
