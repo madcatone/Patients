f1165bafb7b2604610fd08670394e70e11fa2d35
rubyinhell <rubyinhell@gmail.com>
2016-05-16 00:20:31 +0800

    assets plus

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,37 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require uikit
+//= require bootstrap-datetimepicker
 
-function getData(){
-$.ajax({
-  type: 'GET',
-  //url: 'http://iwb.jp/s/js/data.json',
-  //url: '/api/fatigue',
-  url: 'http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f4a75ba9-7721-4363-884d-c3820b0b917c',
-  dataType: 'json',
-  success: function(json){
-   //alert("SUCCESS!!!"+json);
-    var len = json.length;
-    //for(var i=0; i < 10; i++){
-      //alert(json[i].CategoryName);
-      //$("#noti").append(json[i].version + ' ' + json[i].codename + '<br>');
-      //$("#noti").append(json[i].id + ' ' + json[i].sim_number + '<br>'); (Date.parse(s.replace(/-/g, "/"))); 
-      //jsnowtime = new Date();
-      //result = (new Date(Date.parse(nowtime.replace(/-/g, "/"))) - new Date(json[i].current_at) )  / 3600 / 1000;
-      //result = (jsnowtime - new Date(json[i].current_at) )  / 3600 / 1000;
-      //alert(result);
-      //console.log(result);
-      console.log(len);
-      //console.log(json);
-      //if ( result < 0.05 ){ //1.0 = 1hr, 0.1=6min, 0.01 = 18s
-         //nowid = json[i].id;
-         //console.log(result);
-         //console.log(jsnowtime);
-         //console.log(json[i].current_at);
-         //showAlert(result, json[i].sim_number, json[i].current_at);
-      //}//if
-    //}
-  }
+var data = {'data-format': 'yyyy-MM-dd' };
+$(function(){
+    $('.datepicker').attr(data);
+    $('.datepicker').datetimepicker();
 });
-}
-getData();
+
