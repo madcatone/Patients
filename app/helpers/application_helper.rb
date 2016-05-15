aede3a0156d8638328979f44519d52bc325045bf
rubyinhell <rubyinhell@gmail.com>
2016-05-16 00:23:07 +0800

    add useful helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,4 +35,40 @@ module ApplicationHelper
     end
   end
   ##########################
+  def fullname(first, middle, last)
+    "#{first}, #{middle}, #{last}"
+  end
+  def age?(birth_at)
+    now = Time.now.utc.to_date
+    now.year - birth_at.year - (birth_at.to_date.change(:year => now.year) > now ? 1 : 0)
+  end
+  def mr_no(medical_record_no)
+    'MR' + medical_record_no rescue ''
+  end
+  def status(status)
+    case status
+    when '1'
+      I18n.t('patient.initial')
+    when '2'
+      I18n.t('patient.referred')
+    when '3'
+      I18n.t('patient.treatment')
+    when '4'
+      I18n.t('patient.closed')
+    else
+      I18n.t('patient.none')
+    end
+  end
+  def gender(gender)
+    case gender
+    when 'not_available'
+      I18n.t('patient.none')
+    when 'male'
+      I18n.t('form.male')
+    when 'female'
+      I18n.t('form.female')
+    else
+      I18n.t('patient.none')
+    end
+  end
 end
