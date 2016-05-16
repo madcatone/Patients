6700d970e29b5eb3111cdbce0585c4024d2e97f0
rubyinhell <rubyinhell@gmail.com>
2016-05-16 23:56:45 +0800

    add :location to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :translates
   resources :patients
+  resources :locations , only: [:show]
   root 'patients#index'
 
   require 'sidekiq/web'
